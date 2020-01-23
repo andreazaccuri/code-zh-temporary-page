31c6eccc74ec881db47cd0d718f415dd77456ff5
andreazaccuri <24527778+andreazaccuri@users.noreply.github.com>
2020-01-23 11:00:40 +0100

    update js

--- assets/random.js
+++ assets/random.js
@@ -1,8 +1,7 @@
-window.onload = choosePic;
 
-var myPix = new Array("img/b-01.jpg", "img/b-02.jpg", "img/b-03.jpg", "img/b-04.jpg", "img/b-05.jpg", "img/b-06.jpg", "img/b-07.jpg", "img/b-08.jpg", "img/b-09.jpg");
 
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("pic").src = myPix[randomNum];
+const myPix = ["img/b-01.jpg", "img/b-02.jpg", "img/b-03.jpg", "img/b-04.jpg", "img/b-05.jpg", "img/b-06.jpg", "img/b-07.jpg", "img/b-08.jpg", "img/b-09.jpg"];
+window.onload = () => {
+     const randomNum = Math.floor(Math.random() * myPix.length);
+     document.body.style.backgroundImage = `url(${myPix[randomNum]})`;
 }
