968342e3d53e0d160bfaf0c2d5bb7ff8e4b91e42
andreazaccuri <24527778+andreazaccuri@users.noreply.github.com>
2020-01-23 12:51:04 +0100

    style update

--- assets/random.js
+++ assets/random.js
@@ -1,5 +1,5 @@
 const myPix = ["img/b-01.jpg", "img/b-02.jpg", "img/b-03.jpg", "img/b-04.jpg", "img/b-05.jpg", "img/b-06.jpg"];
-const info = ["c1", "c2", "c3", "c4", "c5", "c6"]
+const info = ["caption 1", "caption 2", "caption 3", "caption 4", "caption 5", "caption 6"]
 window.onload = () => {
      const randomNum = Math.floor(Math.random() * myPix.length);
      document.body.style.backgroundImage = `url(${myPix[randomNum]})`;
