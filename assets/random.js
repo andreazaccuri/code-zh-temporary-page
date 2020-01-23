ac970c5602a08f39ed436cdb4d22949ca0aac56e
andreazaccuri <24527778+andreazaccuri@users.noreply.github.com>
2020-01-23 17:50:17 +0100

    update images + captions

--- assets/random.js
+++ assets/random.js
@@ -1,21 +1,16 @@
-const myPix = ["img/b-01.jpg", "img/b-02.jpg", "img/b-03.jpg", "img/b-04.jpg", "img/b-05.jpg", "img/b-06.jpg", "img/b-07.jpg", "img/b-08.jpg", "img/b-09.jpg", "img/b-10.jpg", "img/b-11.jpg", "img/b-12.jpg", "img/b-13.jpg", "img/b-14.jpg", "img/b-15.jpg", "img/b-16.jpg", "img/b-17.jpg"];
-const info = ["Blättler Dafflon, Invitation Card (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)",
-"Realgmnasium Rämibühl Zürich, Printed Matters (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)",
-"Blättler Dafflon, Invitation Card (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)",
-"Frohheim, Magazine (Illustration:&nbsp;&#169;Leander&nbsp;Eisenmann)",
-"Frohheim, Magazine (Illustration:&nbsp;&#169;Anja&nbsp;Peter)",
-"Frohheim, Magazine (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)",
-"anfos/haus, Printed Matter",
-"Frank Ghery On Line, Book",
-"Frank Ghery On Line, Book",
-"Realgmnasium Rämibühl Zürich, Printed Matters (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)",
-"Realgmnasium Rämibühl Zürich, T-Shirt",
-"Very Much Dutch, Poster",
-"anfos/haus, Signage",
-"Riedgraben, Signage",
-"Riedgraben, Signage",
-"Realgmnasium Rämibühl Zürich, Keychain",
-"Park Avenue Armory, Signage"]
+const myPix = ["img/b-01.jpg", "img/b-02.jpg", "img/b-03.jpg", "img/b-04.jpg", "img/b-05.jpg", "img/b-06.jpg", "img/b-07.jpg", "img/b-08.jpg", "img/b-09.jpg", "img/b-10.jpg", "img/b-11.jpg"];
+const info = ["Bl&#228;ttler Dafflon Architekten, Invitation&nbsp;Card (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)", 
+"Bl&#228;ttler Dafflon Architekten, Invitation Card (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)",
+"Baugenossenschaft Frohheim, Magazine 'Frohheim' (Illustration:&nbsp;&#169;Leander&nbsp;Eisenmann)", 
+"UBS Real Estate Management, anfos/haus, Printed Matter",
+"Princeton University Art Museum, Frank Gehry: 'On Line', Book Design", 
+"Princeton University Art Museum, Frank Gehry: 'On Line', Book Design",
+"Realgymnasium R&#228;mib&#252;hl Z&#252;rich, Printed Matters (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)", 
+"Realgmnasium R&#228;mib&#252;hl Z&#252;rich, Give-aways (T-Shirt)",
+"FHNW, Lecture Series: “Very Much Dutch”, Poster", 
+"UBS Real Estate Management, anfos/haus, Signage",
+"Baugenossenschaft Frohheim, Riedgraben, Signage"]
+
 window.onload = () => {
      const randomNum = Math.floor(Math.random() * myPix.length);
      document.body.style.backgroundImage = `url(${myPix[randomNum]})`;
