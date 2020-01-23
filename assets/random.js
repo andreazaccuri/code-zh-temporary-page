5010b67c9f544bdab71c6565bd891fd4a33c372f
andreazaccuri <24527778+andreazaccuri@users.noreply.github.com>
2020-01-23 12:40:13 +0100

    added caption + responsive

--- assets/random.js
+++ assets/random.js
@@ -1,7 +1,7 @@
-
-
-const myPix = ["img/b-01.jpg", "img/b-02.jpg", "img/b-03.jpg", "img/b-04.jpg", "img/b-05.jpg", "img/b-06.jpg", "img/b-07.jpg", "img/b-08.jpg", "img/b-09.jpg"];
+const myPix = ["img/b-01.jpg", "img/b-02.jpg", "img/b-03.jpg", "img/b-04.jpg", "img/b-05.jpg", "img/b-06.jpg"];
+const info = ["c1", "c2", "c3", "c4", "c5", "c6"]
 window.onload = () => {
      const randomNum = Math.floor(Math.random() * myPix.length);
      document.body.style.backgroundImage = `url(${myPix[randomNum]})`;
+     document.getElementById("info-display").innerHTML = info[randomNum];
 }
