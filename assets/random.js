c6738387c289fbc28a2ebd1021e126a1da0fbb89
andreazaccuri <24527778+andreazaccuri@users.noreply.github.com>
2020-01-23 16:18:19 +0100

    new images and captions

--- assets/random.js
+++ assets/random.js
@@ -1,5 +1,21 @@
-const myPix = ["img/b-01.jpg", "img/b-02.jpg", "img/b-03.jpg", "img/b-04.jpg", "img/b-05.jpg", "img/b-06.jpg"];
-const info = ["caption 1", "caption 2", "caption 3", "caption 4", "caption 5", "caption 6"]
+const myPix = ["img/b-01.jpg", "img/b-02.jpg", "img/b-03.jpg", "img/b-04.jpg", "img/b-05.jpg", "img/b-06.jpg", "img/b-07.jpg", "img/b-08.jpg", "img/b-09.jpg", "img/b-10.jpg", "img/b-11.jpg", "img/b-12.jpg", "img/b-13.jpg", "img/b-14.jpg", "img/b-15.jpg", "img/b-16.jpg", "img/b-17.jpg"];
+const info = ["Blättler Dafflon, Invitation Card (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)",
+"Realgmnasium Rämibühl Zürich, Printed Matters (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)",
+"Blättler Dafflon, Invitation Card (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)",
+"Frohheim, Magazine (Illustration:&nbsp;&#169;Leander&nbsp;Eisenmann)",
+"Frohheim, Magazine (Illustration:&nbsp;&#169;Anja&nbsp;Peter)",
+"Frohheim, Magazine (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)",
+"anfos/haus, Printed Matter",
+"Frank Ghery On Line, Book",
+"Frank Ghery On Line, Book",
+"Realgmnasium Rämibühl Zürich, Printed Matters (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)",
+"Realgmnasium Rämibühl Zürich, T-Shirt",
+"Very Much Dutch, Poster",
+"anfos/haus, Signage",
+"Riedgraben, Signage",
+"Riedgraben, Signage",
+"Realgmnasium Rämibühl Zürich, Keychain",
+"Park Avenue Armory, Signage"]
 window.onload = () => {
      const randomNum = Math.floor(Math.random() * myPix.length);
      document.body.style.backgroundImage = `url(${myPix[randomNum]})`;
