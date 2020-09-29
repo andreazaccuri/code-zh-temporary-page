69d921383de952ce22446f939385ede029efe11e
andreazaccuri <24527778+andreazaccuri@users.noreply.github.com>
2020-09-29 14:33:09 +0200

    v2

--- assets/random.js
+++ assets/random.js
@@ -1,20 +1,52 @@
-const myPix = ["img/b-01.jpg", "img/b-02.jpg", "img/b-03.jpg", "img/b-04.jpg", "img/b-05.jpg", "img/b-06.jpg", "img/b-07.jpg", "img/b-08.jpg", "img/b-09.jpg", "img/b-10.jpg", "img/b-11.jpg", "img/b-12.jpg"];
-const info = ["Bl&#228;ttler Dafflon Architekten, Invitation&nbsp;Card (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)", 
-"Bl&#228;ttler Dafflon Architekten, Invitation Card (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)",
-"Baugenossenschaft Frohheim, Magazine 'Frohheim' (Illustration:&nbsp;&#169;Leander&nbsp;Eisenmann)", 
-"UBS Real Estate Management, anfos/haus, Printed Matter",
-"Princeton University Art Museum, Frank Gehry: 'On Line', Book Design", 
-"Princeton University Art Museum, Frank Gehry: 'On Line', Book Design",
-"Realgymnasium R&#228;mib&#252;hl Z&#252;rich, Printed Matters (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)", 
-"Realgymnasium R&#228;mib&#252;hl Z&#252;rich, Give-aways (T-Shirt)",
-"FHNW, Lecture Series: “Very Much Dutch”, Poster", 
-"UBS Real Estate Management, anfos/haus, Signage",
-"Baugenossenschaft Frohheim, Riedgraben, Signage (Photography:&nbsp;&#169;Saskja&nbsp;Rosset)", "Collegium Novum Z&#252;rich, Identity + Printed Matter + Website"]
+const myPix = [
+     "img/b-01.jpg", 
+     "img/b-02.jpg", 
+     "img/b-03.jpg", 
+     "img/b-04.jpg", 
+     "img/b-05.jpg", 
+     "img/b-06.jpg", 
+     "img/b-07.jpg", 
+     "img/b-08.jpg", 
+     "img/b-09.jpg", 
+     "img/b-10.jpg", 
+     "img/b-11.jpg", 
+     "img/b-12.jpg", 
+     "img/b-13.jpg"];
+ 
+ const info = [
+     "Bl&#228;ttler Dafflon Architekten, (Photography: &#169;Saskja Rosset)", 
+     "Bl&#228;ttler Dafflon Architekten, (Photography: &#169;Saskja Rosset)",
+     "Baugenossenschaft Frohheim, Magazine 'Frohheim' (Illustration:&nbsp;&#169;Leander&nbsp;Eisenmann)", 
+     "UBS Real Estate Management, anfos/haus",
+     "Princeton University Art Museum, Frank Gehry: 'On Line'", 
+     "Princeton University Art Museum, Frank Gehry: 'On Line'",
+     "Realgymnasium R&#228;mib&#252;hl Z&#252;rich (Photography: &#169;Saskja Rosset)", 
+     "Realgymnasium R&#228;mib&#252;hl Z&#252;rich (T-Shirt)",
+     "FHNW, Lecture Series: “Very Much Dutch”", 
+     "UBS Real Estate Management, anfos/haus",
+     "Baugenossenschaft Frohheim, Riedgraben (Photography: &#169;Saskja Rosset)", 
+     "Collegium Novum Z&#252;rich",
+     "Collegium Novum Z&#252;rich"];
+ 
+ const cat = [ "Invitation Card",
+     "Invitation Card",+
+     "Art Direction + Editors + Graphic Design",
+     "Printed Matter + Signage",
+     "Book Design",
+     "Book Design",
+     "Printed Matters",
+     "Give-aways",
+     "Poster",
+     "Signage",
+     "Signage",
+     "Identity + Printed Matter + Website",
+     "Identity + Printed Matter + Website"];
 
 window.onclick = () => {
      const randomNum = Math.floor(Math.random() * myPix.length);
      document.body.style.backgroundImage = `url(${myPix[randomNum]})`;
      document.getElementById("info-display").innerHTML = info[randomNum];
+     document.getElementById("category").innerHTML = cat[randomNum];
 }
 
 targetElement.ontouchend = (e) => {
